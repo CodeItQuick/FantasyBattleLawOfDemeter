35337ecf91b08dba4e8e0217281cf2e3572ac1c8
Dan Nguyen <dan@august.com>
2024-02-12 13:15:54 -0800

    add test with simple enemy soak

--- tests/Player.spec.ts
+++ tests/Player.spec.ts
@@ -50,4 +50,42 @@ describe('Player', () => {
             .calculateDamage(enemy);
         expect(damage.amount).toBe(10);
     });
+
+    it('calculates damage with sword with 5 simple armor on Simple Enemy', () => {
+        const inventory = new Inventory(
+            new Equipment(
+                new BasicItem("Flash Sword of Danger", 10, 1),
+                new BasicItem("Shield", 0, 0),
+                new BasicItem("Helmet", 0, 0),
+                new BasicItem("Shoes", 0, 0),
+                new BasicItem("Chestplate", 0, 0)
+            ));
+        const stats = new Stats(0);
+        const enemy = new SimpleEnemy(
+            new SimpleArmor(5),
+            [new BasicBuff(0, 0)]);
+
+        const damage: Damage = new Player(inventory, stats)
+            .calculateDamage(enemy);
+        expect(damage.amount).toBe(5);
+    });
+
+    it('calculates damage with sword with 5 simple armor on Simple Enemy, with soak 0', () => {
+        const inventory = new Inventory(
+            new Equipment(
+                new BasicItem("Flash Sword of Danger", 10, 1),
+                new BasicItem("Shield", 0, 0),
+                new BasicItem("Helmet", 0, 0),
+                new BasicItem("Shoes", 0, 0),
+                new BasicItem("Chestplate", 0, 0)
+            ));
+        const stats = new Stats(0);
+        const enemy = new SimpleEnemy(
+            new SimpleArmor(1),
+            [new BasicBuff(4, 0)]);
+
+        const damage: Damage = new Player(inventory, stats)
+            .calculateDamage(enemy);
+        expect(damage.amount).toBe(5);
+    });
 })
