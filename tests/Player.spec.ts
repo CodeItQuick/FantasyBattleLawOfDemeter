7aefa4d17d492eb86a92dbcf723a2133c5b9142d
Dan Nguyen <dan@august.com>
2024-02-12 13:22:10 -0800

    add test variations with BasicItems

--- tests/Player.spec.ts
+++ tests/Player.spec.ts
@@ -88,4 +88,58 @@ describe('Player', () => {
             .calculateDamage(enemy);
         expect(damage.amount).toBe(5);
     });
+
+    it('calculates damage with sword with 1 shield', () => {
+        const inventory = new Inventory(
+            new Equipment(
+                new BasicItem("Flash Sword of Danger", 10, 1),
+                new BasicItem("Shield", 1, 0),
+                new BasicItem("Helmet", 0, 0),
+                new BasicItem("Shoes", 0, 0),
+                new BasicItem("Chestplate", 0, 0)
+            ));
+        const stats = new Stats(0);
+        const enemy = new SimpleEnemy(
+            new SimpleArmor(0), []);
+
+        const damage: Damage = new Player(inventory, stats)
+            .calculateDamage(enemy);
+        expect(damage.amount).toBe(11);
+    });
+
+    it('calculates damage with sword with 1 shield', () => {
+        const inventory = new Inventory(
+            new Equipment(
+                new BasicItem("Flash Sword of Danger", 10, 1),
+                new BasicItem("Shield", 0, 2),
+                new BasicItem("Helmet", 0, 0),
+                new BasicItem("Shoes", 0, 0),
+                new BasicItem("Chestplate", 0, 0)
+            ));
+        const stats = new Stats(0);
+        const enemy = new SimpleEnemy(
+            new SimpleArmor(0), []);
+
+        const damage: Damage = new Player(inventory, stats)
+            .calculateDamage(enemy);
+        expect(damage.amount).toBe(30);
+    });
+
+    it('calculates damage with sword with variations with different items and attributes', () => {
+        const inventory = new Inventory(
+            new Equipment(
+                new BasicItem("Flash Sword of Danger", 10, 1),
+                new BasicItem("Shield", 0, 0),
+                new BasicItem("Helmet", 1, 0),
+                new BasicItem("Shoes", 0, 1),
+                new BasicItem("Chestplate", 0, 0)
+            ));
+        const stats = new Stats(0);
+        const enemy = new SimpleEnemy(
+            new SimpleArmor(0), []);
+
+        const damage: Damage = new Player(inventory, stats)
+            .calculateDamage(enemy);
+        expect(damage.amount).toBe(22);
+    });
 })
